1bd1cd6f2675132fae6fe1335eac118abf84e20f
Mathias Rav <rav@cs.au.dk>
2014-03-22 18:19:13 +0100

    Support multiple dimensions

--- poly.py
+++ poly.py
@@ -1,19 +1,53 @@
 import sympy
 
-roots = (-1, 1, 4, 6)
-degree = 4
+roots = ((-1, -1), (0, 0), (1, 2))
+degree = 2
+dims = 2
 
-a = tuple(tuple(x ** d for d in range(degree+1)) for x in roots)
-b = tuple(0 for each in range(degree+1))
-cs = tuple(sympy.symbols('c%d' % d) for d in range(degree+1))
+if dims <= 3:
+    names = ('x', 'y', 'z')
+    xs = sympy.symbols(names[:dims])
+else:
+    xs = sympy.symbols(tuple('x%d' % n for n in range(1, dims+1)))
+
+def sum_to(n, degree):
+    if n == 1:
+        yield (degree,)
+        return
+    for d in range(degree+1):
+        for p in sum_to(n-1, degree-d):
+            yield (d,) + p
+
+def monomials(xs, degree):
+    for d in range(degree+1):
+        for exps in sum_to(len(xs), d):
+            p = 1
+            for x, e in zip(xs, exps):
+                p *= x ** e
+            yield p
+
+def coefficients(n, degree):
+    symbols = []
+    for d in range(degree+1):
+        for exps in sum_to(n, degree):
+            symbols.append('c%s' % ''.join('_%d' % e for e in exps))
+    return tuple(sympy.symbols(symbols))
+
+a = tuple(tuple(monomials(x, degree)) for x in roots)
+b = tuple(0 for each in roots)
+cs = tuple(coefficients(dims, degree))
 
 eqns = tuple(sympy.Eq(sum(ci * ai for ci, ai in zip(cs, row)), bi) for row, bi in zip(a, b))
+print("Linear system:")
 print(eqns)
 sol = sympy.solve(eqns, cs)
+print("Solution(s):")
+print(sol)
 
-x = sympy.symbols('x')
-p = sum(sol.get(ci, ci) * x ** d for ci, d in zip(cs, range(degree+1)))
+p = sum(sol.get(ci, ci) * m for ci, m in zip(cs, monomials(xs, degree)))
+p = p.subs((ci, 1) for ci in cs)
+print("Polynomium:")
 print(sympy.Eq(p, 0))
 for xi in roots:
-    print("Check root %d:" % xi)
-    print(p.subs(x, xi))
+    print("Check root %s:" % (xi,))
+    print(p.subs(zip(xs, xi)))
