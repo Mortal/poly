e7aa8459c1323c62b8910c49ad8786764db6cd07
Mathias Rav <rav@cs.au.dk>
2014-03-23 13:06:17 +0100

    wip

--- poly.py
+++ poly.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
 import sympy
+import numpy as np
 
-roots = ((-1, -1), (0, 0), (1, 2))
+roots = ((0, 0), (2, 0), (0, 2), (2, 2))
 degree = 2
 dims = 2
 
@@ -30,7 +32,8 @@ def coefficients(n, degree):
     symbols = []
     for d in range(degree+1):
         for exps in sum_to(n, degree):
-            symbols.append('c%s' % ''.join('_%d' % e for e in exps))
+            e_str = '%d' if degree < 10 else '_%d'
+            symbols.append('c%s' % ''.join(e_str % e for e in exps))
     return tuple(sympy.symbols(symbols))
 
 a = tuple(tuple(monomials(x, degree)) for x in roots)
@@ -46,8 +49,39 @@ print(sol)
 
 p = sum(sol.get(ci, ci) * m for ci, m in zip(cs, monomials(xs, degree)))
 p = p.subs((ci, 1) for ci in cs)
+
+fp = sympy.lambdify(xs, p, modules='numpy')
 print("Polynomium:")
 print(sympy.Eq(p, 0))
 for xi in roots:
     print("Check root %s:" % (xi,))
-    print(p.subs(zip(xs, xi)))
+    print(fp(*xi))
+
+x1 = min(root[0] for root in roots)
+x2 = max(root[0] for root in roots)
+y1 = min(root[1] for root in roots)
+y2 = max(root[1] for root in roots)
+
+w = x2 - x1
+h = y2 - y1
+
+if w < 1e-6:
+    w = 1
+if h < 1e-6:
+    h = 1
+
+x1 -= w/10
+x2 += w/10
+y1 -= h/10
+y2 += h/10
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+xs = np.linspace(x1, x2, 200)
+ys = np.linspace(y1, y2, 200)
+xy = np.meshgrid(xs, ys)
+ax.pcolormesh(xy[0], xy[1], np.minimum(1, np.maximum(-1, fp(xy[0], xy[1]))))
+rootx, rooty = zip(*roots)
+ax.plot(rootx, rooty, 'o')
+
+plt.show()
